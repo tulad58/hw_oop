a0a7e38fc49924e65e69de1251998128d80a31af
Tulad95 <link7800@yandex.ru>
2023-03-08 20:37:02 +0600

    задание 3

--- hw1.py
+++ hw1.py
@@ -16,6 +16,25 @@ class Student:
         else:
             return 'Ошибка'
 
+    def __str__(self):
+        print(f"Имя: {self.name}\n"
+              f"Фамилия: {self.surname}\n"
+              f"Средняя оценка за оекцию: {self.avarage_grade()}\n"
+              f"Курсы в процессе изучения: {self.courses_in_progress}\n"
+              f"Завершенные курсы: {self.finished_courses}")
+
+    def avarage_grade(self):
+        return sum(self.grades.values()) / len(self.grades)
+
+    def __lt__(self, other):
+        if isinstance(other, Student):
+            if self.avarage_grade() < other.avarage_grade():
+                return f"{other.name}"
+            else:
+                return f"{self.name}"
+
+
+
 class Mentor:
     def __init__(self, name, surname):
         self.name = name
@@ -28,6 +47,19 @@ class Lecturer(Mentor):
     def __init__(self, name, surname):
         super().__init__(name, surname)
         self.grades = {}
+
+    def __str__(self):
+        print(f"Имя: {self.name}\nФамилия: {self.surname}\nСредняя оценка за оекцию: {self.avarage_grade()}")
+
+    def avarage_grade(self):
+        return sum(self.grades.values()) / len(self.grades)
+
+    def __lt__(self, other):
+        if isinstance(other, Lecturer):
+            if self.avarage_grade() < other.avarage_grade():
+                return f"{other.name}"
+            else:
+                return f"{self.name}"
 
 
 class Reviewer(Mentor):
@@ -42,4 +74,8 @@ class Reviewer(Mentor):
                 student.grades[course] = [grade]
         else:
             return 'Ошибка'
+
+    def __str__(self):
+        print(f"Имя: {self.name}\nФамилия: {self.surname}")
+
 
